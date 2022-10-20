800347d9d55d0ebc76ed00c0a13a7cbaba2823d7
Simão sá <simaosa@Simaos-MacBook-Pro.local>
2022-10-20 16:52:08 +0100

    changed test url and code accordingly

--- test/snapshot_tests/util_constants.js
+++ test/snapshot_tests/util_constants.js
@@ -2,20 +2,20 @@ export const ONE_SECOND = 1000;
 export const FIVE_SECONDS = 5000;
 export const TEN_SECONDS = 10000;
 export const ONE_MINUTE = 60000;
-export const baseURL = 'https://open-physiology-viewer-tgbugs.surge.sh/'
-// export const baseURL = 'https://open-physiology-viewer.surge.sh/'
+// export const baseURL = 'https://open-physiology-viewer-tgbugs.surge.sh/'
+export const baseURL = 'https://open-physiology-viewer.surge.sh/'
 
 
 export const scaffoldGroupName = [
-    'F anchors',
-    'D anchors',
-    'N anchors',
-    'F wires',
-    'D wires',
-    'N wires',
-    'F regions',
-    'D regions',
-    'N regions'
+    'too:F anchors',
+    'too:D anchors',
+    'too:N anchors',
+    'too:F wires',
+    'too:D wires',
+    'too:N wires',
+    'too:F regions',
+    'too:D regions',
+    'too:N regions'
 ]
 
 
