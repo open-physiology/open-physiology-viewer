200488a77f8d49a9dca9735910c33b1a56303208
Simão sá <simaosa@Simaos-MacBook-Pro.local>
2022-10-20 17:47:46 +0100

    changed expect condition to be more general

--- test/snapshot_tests/util_constants.js
+++ test/snapshot_tests/util_constants.js
@@ -2,25 +2,25 @@ export const ONE_SECOND = 1000;
 export const FIVE_SECONDS = 5000;
 export const TEN_SECONDS = 10000;
 export const ONE_MINUTE = 60000;
+export const HALF_SECOND = 500
 // export const baseURL = 'https://open-physiology-viewer-tgbugs.surge.sh/'
 export const baseURL = 'https://open-physiology-viewer.surge.sh/'
 
 
 export const scaffoldGroupName = [
-    'too:F anchors',
-    'too:D anchors',
-    'too:N anchors',
-    'too:F wires',
-    'too:D wires',
-    'too:N wires',
-    'too:F regions',
-    'too:D regions',
-    'too:N regions'
+    'F anchors',
+    'D anchors',
+    'N anchors',
+    'F wires',
+    'D wires',
+    'N wires',
+    'F regions',
+    'D regions',
+    'N regions'
 ]
 
 
 export const KeastSpinalModelGroups = [
-    'Ungrouped',
     'Sympathetic chain',
     'Axon',
     'Dendrite'
