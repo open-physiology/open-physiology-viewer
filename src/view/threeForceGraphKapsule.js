72aa654e5cca560787e614b10c66d949d6b29d27
jrmartin <yeshuarmartinez@gmail.com>
2022-11-30 09:44:57 -0800

    Cleanup #175

--- src/view/threeForceGraphKapsule.js
+++ src/view/threeForceGraphKapsule.js
@@ -197,12 +197,10 @@ export default Kapsule({
         d3VelocityDecay  : { default: 0.45}, //triggerUpdate: false, onChange(velocityDecay, state) { state.simulation.velocityDecay(velocityDecay) } },
 
         warmupTicks      : { default: 10 }, // how many times to tick the force engine at init before starting to render
-        cooldownTicks    : { default: 10 },
+        cooldownTicks    : { default: 100 },
         cooldownTime     : { default: 1000 }, // in milliseconds. Graph UI Events  need wait for this period of time before  webgl interaction is processed. (E.g. hideHighlighted() in WebGLComponent.)
         onLoading        : { default: () => {}, triggerUpdate: false },
         onFinishLoading  : { default: () => {}, triggerUpdate: false },
-
-        neuroviewEnabled      : { default: false},
 
         enablePointerInteraction: {
             default: true,
