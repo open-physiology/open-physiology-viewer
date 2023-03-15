d3c7163e56e7601bd0bf554d7da2aff700a65057
Filippo Ledda <filippo@metacell.us>
2023-03-15 11:33:10 +0100

    Fix test docker image

--- test/snapshot_tests/util_constants.js
+++ test/snapshot_tests/util_constants.js
@@ -4,7 +4,7 @@ export const TEN_SECONDS = 10000;
 export const ONE_MINUTE = 60000;
 export const HALF_SECOND = 500
 // export const baseURL = 'https://open-physiology-viewer.surge.sh/'
-export const baseURL = 'http://app:80'
+export const baseURL = process.env.TEST_URL || 'https://open-physiology-viewer.surge.sh/'
 
 export const TOO_MAP_MODEL_LINK = 'https://raw.githubusercontent.com/open-physiology/apinatomy-models/master/models/too-map/source/too-map.json'
 export const KEAST_SPINAL_TEST_MODEL_LINK = 'https://raw.githubusercontent.com/open-physiology/open-physiology-viewer/master/test/data/keastSpinalTest.json'
